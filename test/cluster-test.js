8806c9a8143abcf0a5bc25b6a82cb9d373c49d63
Subbu <sallamaraju@ebaysf.com>
2012-04-26 12:14:59 -0700

    Add more tests

--- test/cluster-test.js
+++ test/cluster-test.js
@@ -179,7 +179,100 @@ module.exports = {
             test.done();
         });
         start(emitter);
+    },
+
+    'start, graceful shutdown': function (test) {
+        var emitter = new EventEmitter();
+
+        emitter.on('starting', function () {
+            waitForStart(emitter, test, 0, 100);
+        });
+
+        var respCount = 0;
+        emitter.on('started', function () {
+            for(var i = 0; i < 2000; i++) {
+                request('http://localhost:3000', function (error, response, body) {
+                    if(error) {
+                        test.ok(false, 'got error from server')
+                    }
+                    else {
+                        respCount++;
+                    }
+                });
+            }
+            // Send shutdown while requests are in-flight
+            shutdown(emitter);
+        });
+
+        emitter.on('start failure', function (error) {
+            log('Failed to start');
+            log(error.stack || error);
+            test.ok(false, 'failed to start')
+        });
+
+        emitter.on('stopping', function () {
+            waitForStop.apply(null, [emitter, test, 0, 100])
+        });
+
+        emitter.on('stopped', function () {
+            // Ensure that all in-flight requests are handled
+            test.equals(respCount, 2000);
+            log('Stopped');
+            // Assert that there are 0 pids.
+            fs.readdir('./pids', function (err, paths) {
+                test.equal(paths.length, 0);
+            });
+            test.done();
+        });
+        start(emitter);
+    },
+
+    'start, abrupt stop': function (test) {
+        var emitter = new EventEmitter();
+
+        emitter.on('starting', function () {
+            waitForStart(emitter, test, 0, 100);
+        });
+
+        var respCount = 0, errCount = 0;
+        emitter.on('started', function () {
+            for(var i = 0; i < 2000; i++) {
+                request('http://localhost:3000', function (error, response, body) {
+                    if(error) {
+                        errCount++;
+                    }
+                    else {
+                        respCount++;
+                    }
+                });
+            }
+            // Send shutdown while requests are in-flight
+            stop(emitter);
+        });
+
+        emitter.on('start failure', function (error) {
+            log('Failed to start');
+            log(error.stack || error);
+            test.ok(false, 'failed to start')
+        });
+
+        emitter.on('stopping', function () {
+            waitForStop.apply(null, [emitter, test, 0, 100])
+        });
+
+        emitter.on('stopped', function () {
+            // Ensure that all in-flight requests are handled
+            test.ok(respCount + errCount, 2000);
+            log('Stopped');
+            // Assert that there are 0 pids.
+            fs.readdir('./pids', function (err, paths) {
+                test.equal(paths.length, 0);
+            });
+            test.done();
+        });
+        start(emitter);
     }
+
 }
 
 // Start the cluster
