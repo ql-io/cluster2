37e2f54f89adc4fed3bdfb88ba89a8df89b66b2b
Subbu <sallamaraju@ebaysf.com>
2012-04-26 11:36:29 -0700

    Enable debug

--- test/cluster-test.js
+++ test/cluster-test.js
@@ -22,7 +22,7 @@ var childProcess = require('child_process'),
     os = require('os'),
     EventEmitter = require('events').EventEmitter;
 
-var debug = false;
+var debug = true;
 function log() {
     if(debug) {
         console.log.apply(null, arguments);
